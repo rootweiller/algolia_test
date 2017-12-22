e33c321e2df61d89488a3e609ac50472e4f8a9ea
Juan Vargas <rootweiller83@gmail.com>
2017-12-22 14:46:06 -0500

    Add config file

---
+++ instance/config.py
@@ -0,0 +1,14 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Debug True
+DEBUG = True
+
+# Algolia KEY
+APP_ID = "APP_ID"
+API_KEY = "API_KEY"
+INDEX_NAME = "INDEX_NAME"
+
+# CSV URL DOWNLOAD
+XML_URL = "XML_URL"
