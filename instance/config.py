242f34bed9ca07c2ffd4803e906a634f59020655
Juan Vargas <rootweiller83@gmail.com>
2017-12-22 18:14:31 -0500

    refactoring config file

--- instance/config.py
+++ instance/config.py
@@ -1,14 +1,25 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-# Debug True
-DEBUG = True
+class BaseConfig:
 
-# Algolia KEY
-APP_ID = "APP_ID"
-API_KEY = "API_KEY"
-INDEX_NAME = "INDEX_NAME"
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-# CSV URL DOWNLOAD
-XML_URL = "XML_URL"
+    # Debug True
+    DEBUG = False
+
+    # Algolia KEY
+    APP_ID = "7UHV2993DH"
+    API_KEY = "40645e7932ca76222885dcd8f1cf0f53"
+    INDEX_NAME = "SDN"
+
+    # CSV URL DOWNLOAD
+    XML_URL = "https://www.treasury.gov/ofac/downloads/add.csv"
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
